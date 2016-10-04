a9911f432a561c9878936195e5aacca7f2d20af4
m4b3l <mabel.yes@hotmail.com>
2016-10-03 23:23:52 -0400

    archivoController adition

--- JavaFootBallBoard/src/javafootballboard/JavaFootBallBoard.java
+++ JavaFootBallBoard/src/javafootballboard/JavaFootBallBoard.java
@@ -3,14 +3,16 @@ package javafootballboard;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafootballboard.Controller.Archivos;
 import javafootballboard.Model.*;
 import javafootballboard.View.*;
 
 public class JavaFootBallBoard {
-
+    
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.setVisible(true);
